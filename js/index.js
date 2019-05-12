26a93d29ec42481a9db86945237b1e11c0ce2d93
FrankYeah <aaa24295234@gmail.com>
2019-05-12 22:38:41 +0800

    menu

--- js/index.js
+++ js/index.js
@@ -27,4 +27,55 @@ $(document).ready(function(){
       
       })
 
+    // menu 開關
+    $('#page_one_close_div').click(function(){
+      $('#page_one_menu_click_bar').css('display','none');
+    })
+
+    $('#page_one_menu_icon').click(function(){
+      $('#page_one_menu_click_bar').css('display','block');
+    })
+
+    // mouseover
+    $('.page-one-content-one').mouseover(function(){
+      $('.page-one-menu-arrow-one').css('display','block');
+    })
+
+    $('.page-one-content-one').mouseleave(function(){
+      $('.page-one-menu-arrow-one').css('display','none');
+    })
+
+    $('.page-one-content-two').mouseover(function(){
+      $('.page-one-menu-arrow-two').css('display','block');
+    })
+
+    $('.page-one-content-two').mouseleave(function(){
+      $('.page-one-menu-arrow-two').css('display','none');
+    })
+
+    $('.page-one-content-three').mouseover(function(){
+      $('.page-one-menu-arrow-three').css('display','block');
+    })
+
+    $('.page-one-content-three').mouseleave(function(){
+      $('.page-one-menu-arrow-three').css('display','none');
+    })
+
+    $('.page-one-content-four').mouseover(function(){
+      $('.page-one-menu-arrow-four').css('display','block');
+    })
+
+    $('.page-one-content-four').mouseleave(function(){
+      $('.page-one-menu-arrow-four').css('display','none');
+    })
+
+    $('.page-one-content-five').mouseover(function(){
+      $('.page-one-menu-arrow-five').css('display','block');
+    })
+
+    $('.page-one-content-five').mouseleave(function(){
+      $('.page-one-menu-arrow-five').css('display','none');
+    })
+
+
   });
