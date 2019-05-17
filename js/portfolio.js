159d26d1bf8ccf8e9d3e9cd9e153b458bd9ccb14
FrankYeah <aaa24295234@gmail.com>
2019-05-17 18:38:16 +0800

    new

---
+++ js/portfolio.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    // menu 開關
+    $('#right_close_div').click(closeMenu)
+    $('#right_menu_icon').click(openMenu)
+
+    function openMenu(){
+      $('#right_menu_click_bar').css('visibility','visible');
+      $('#right_menu_click_bar').css('opacity','1');
+      $('#right_menu_click_bar').css('width','54%');
+      $('#right_menu_click_bar').css('transition','width 0.6s ease 0s');
+    }
+
+    function closeMenu(){
+      setTimeout(function(){
+        $('#right_menu_click_bar').css('visibility','hidden')
+      },600)
+      $('#right_menu_click_bar').css('opacity','0.9');
+      $('#right_menu_click_bar').css('width','10%');
+      $('#right_menu_click_bar').css('transition','width 0.6s ease 0s');
+    }
+
+
+
+
+
+
+
+})
